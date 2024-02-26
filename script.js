0646d04fd09afab3f57f317742a61f0ee48cdef9
FuluZwivhuya <148289949+FuluZwivhuya@users.noreply.github.com>
2024-02-26 20:51:41 +0200

    practice

---
+++ script.js
@@ -0,0 +1,18 @@
+const example = 10
+
+console.log(example + example )
+console.log(`The number is ${example }`)
+console.log(100 - example )
+
+const greet =()=> {
+    console.log("hello world!")
+}
+
+greet()
+
+const bark =()=>{
+    console.log("woof")
+}
+ bark()
+ bark()
+ bark()
