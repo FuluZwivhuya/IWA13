7383a6f16a9a49ac420c30117cab2c0e6b8dce78
FuluZwivhuya <148289949+FuluZwivhuya@users.noreply.github.com>
2024-02-27 21:48:15 +0200

    changes to be made on functions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const example = 10
+/*const example = 10
 
 console.log(example + example )
 console.log(`The number is ${example }`)
@@ -15,4 +15,51 @@ const bark =()=>{
 }
  bark()
  bark()
- bark()
+ bark()*/
+
+let state = 'idle'
+let user = null
+let calculated = '1'
+
+// Only allowed to change below
+
+
+const logCalc = () => { 
+    const instring = typeof calculated === 'numerical-string' 
+    const calculatedAsNumber = parseInt(calculated)
+    calculated === calculatedAsNumber + 1 
+}
+
+console.log(logCalc)
+
+const calcUser = () => {
+  logCalc
+  if (calculated > 2) user = 'John'
+  if (calculated > 2) state = 'requesting'
+  if (calculated > 3) state = 'idle'
+}
+console.log(calcUser)
+console.log(user)
+
+const checkUser = () => {
+	if (user && state === 'requesting') {
+		console.log(user)
+	}
+}
+
+// Only allowed to change code above
+
+checkUser()
+calcUser()
+
+checkUser()
+calcUser()
+
+checkUser()
+calcUser()
+
+checkUser()
+calcUser()
+
+checkUser()
+calcUser()
