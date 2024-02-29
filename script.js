4abd7a675bee86d81f9f774ff50785011b5943ff
FuluZwivhuya <148289949+FuluZwivhuya@users.noreply.github.com>
2024-02-29 20:03:32 +0200

    to work on func

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-/*const example = 10
 
-console.log(example + example )
-console.log(`The number is ${example }`)
-console.log(100 - example )
-
-const greet =()=> {
-    console.log("hello world!")
-}
-
-greet()
-
-const bark =()=>{
-    console.log("woof")
-}
- bark()
- bark()
- bark()*/
 
 let state = 'idle'
 let user = null
@@ -24,28 +7,25 @@ let calculated = '1'
 // Only allowed to change below
 
 
-const logCalc = () => { 
-    const instring = typeof calculated === 'numerical-string' 
+function logCalc (){  
     const calculatedAsNumber = parseInt(calculated)
     calculated === calculatedAsNumber + 1 
 }
 
-console.log(logCalc)
 
-const calcUser = () => {
+function calcUser () {
   logCalc
-  if (calculated > 2) user = 'John'
-  if (calculated > 2) state = 'requesting'
-  if (calculated > 3) state = 'idle'
+  if (parseInt(calculated) > 2) user === 'John'
+  if (parseInt(calculated) > 2) state === 'requesting'
+  if (parseInt(calculated) > 3) state === 'idle'
 }
-console.log(calcUser)
-console.log(user)
 
-const checkUser = () => {
-	if (user && state === 'requesting') {
+function checkUser () {
+	 (user && state === 'requesting') 
 		console.log(user)
 	}
-}
+
+
 
 // Only allowed to change code above
 
