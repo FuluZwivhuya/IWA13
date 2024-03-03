bda0242f661c6b1a77b1b75992a1afc040950023
FuluZwivhuya <148289949+FuluZwivhuya@users.noreply.github.com>
2024-03-03 20:13:47 +0200

    still not sure what is happening

--- script.js
+++ script.js
@@ -7,23 +7,26 @@ let calculated = '1'
 // Only allowed to change below
 
 
-function logCalc (){  
-    const calculatedAsNumber = parseInt(calculated)
-    calculated === calculatedAsNumber + 1 
+function logCalc (){ 
+   calculated = typeof string === 'numerical-string';
+   let calculatedAsNumber = (parseInt(calculated));
+   return calculated = calculatedAsNumber + 1;
 }
-
 
 function calcUser () {
   logCalc
-  if (parseInt(calculated) > 2) user === 'John'
-  if (parseInt(calculated) > 2) state === 'requesting'
-  if (parseInt(calculated) > 3) state === 'idle'
+  if (calculated = 2) user = 'john'
+  if (calculated = 2) state = 'requesting';
+  if (calculated = 3) state = 'idle';
+  return user,state
 }
 
 function checkUser () {
-	 (user && state === 'requesting') 
-		console.log(user)
-	}
+  let user = 'john';
+  if (user && state === 'requesting');
+  console.log(`User: ${user} (${calculated})`)
+}
+
 
 
 
