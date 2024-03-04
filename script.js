8992dc12c689282b4b32be82e37fca93a13a79d7
FuluZwivhuya <148289949+FuluZwivhuya@users.noreply.github.com>
2024-03-04 19:34:14 +0200

    still unusual

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
-
-
-let state = 'idle'
-let user = null
-let calculated = '1'
+let state = "idle";
+let user = null;
+let calculated = "1";
 
 // Only allowed to change below
 
-
-function logCalc (){ 
-   calculated = typeof string === 'numerical-string';
-   let calculatedAsNumber = (parseInt(calculated));
-   return calculated = calculatedAsNumber + 1;
+function logCalc() {
+  calculated = typeof string === "numerical-string";
+  let calculatedAsNumber = parseInt(calculated);
+  calculated = calculatedAsNumber + 1;
 }
 
-function calcUser () {
-  logCalc
-  if (calculated = 2) user = 'john'
-  if (calculated = 2) state = 'requesting';
-  if (calculated = 3) state = 'idle';
-  return user,state
+function calcUser() {
+  logCalc();
+  if (calculated = 2) {
+    user = "john";
+  }
+  if (calculated = 2) {
+    state = "requesting";
+  }
+  if (calculated = 3) {
+    state = "idle";
+  }
 }
 
-function checkUser () {
-  let user = 'john';
-  if (user && state === 'requesting');
-  console.log(`User: ${user} (${calculated})`)
+function checkUser() {
+  if (user && state === "requesting");
+  console.log(`User: ${user} (${calculated})`);
 }
-
 
 // Only allowed to change code above
 
-checkUser()
-calcUser()
+checkUser();
+calcUser();
 
-checkUser()
-calcUser()
+checkUser();
+calcUser();
 
-checkUser()
-calcUser()
+checkUser();
+calcUser();
 
-checkUser()
-calcUser()
+checkUser();
+calcUser();
 
-checkUser()
-calcUser()
+checkUser();
+calcUser();
